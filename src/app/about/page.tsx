35826f6fb12b443366d3d83c3923eb3631aa7673
Prince Kumar <thecoder9709@gmail.com>
2025-05-26 08:11:16 +0530

    modify aboutpage
    add new about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+// import AboutSectionOne from "@/components/About/AboutSectionOne";
+// import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import AboutUs from "@/components/About/ModifyAbout";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -17,8 +18,7 @@ const AboutPage = () => {
         pageName="About Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+      <AboutUs />
     </>
   );
 };
