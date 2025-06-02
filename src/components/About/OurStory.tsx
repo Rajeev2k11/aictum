6d1d2f375ee0ff111d56c599f793fb77048bef2e
Rajeev Ranjan Singh <rajeevranjan2k11@gmail.com>
2025-06-03 01:00:04 +0530

    Fixed

--- src/components/About/OurStory.tsx
+++ src/components/About/OurStory.tsx
@@ -6,14 +6,24 @@ export const OurStory = () => (
       <div className="flex-1">
         <h3 className="text-3xl font-medium mb-8 text-[#AE69DF]">Our Story</h3>
         <p className="text-black mb-6 leading-relaxed">
-          Founded in 2003 as a small startup with just three engineers, we've grown into a multinational 
-          technology leader through relentless innovation and customer-focused solutions. Our journey has 
-          been marked by milestones that reflect our commitment to pushing technological boundaries.
+        At <span className='font-bold'>AICTUM</span>, we believe that the digital future belongs to those who innovate boldly and execute 
+        precisely. Founded with the vision of seamlessly integrating the transformative powers of
+        <span className='font-bold'>Artificial Intelligence</span> and  <span className='font-bold'>Blockchain Technology</span>, AICTUM is dedicated to delivering 
+         excellence and empowering businesses to thrive in a rapidly evolving digital landscape.
         </p>
         <p className="text-black leading-relaxed">
-          What sets us apart is our culture of continuous learning and our belief that technology should 
-          be both powerful and accessible. We invest heavily in research and development, with 30% of our 
-          revenue dedicated to creating the next generation of solutions.
+        Our journey began with a simple yet powerful ambition—to make complex technologies 
+        accessible, reliable, and impactful. From early challenges to strategic breakthroughs, 
+        we've consistently emphasized collaboration, innovation, and transparency.
+        </p>
+
+        <p className="text-black leading-relaxed">
+        Today, AICTUM proudly partners with forward-thinking businesses, helping them navigate 
+        and dominate through intelligent solutions tailored specifically to their unique needs.
+         Driven by passion, guided by expertise, and committed to excellence, we invite you to become a part of our evolving story.<br />
+
+<span className='font-bold '>Together, let's shape a smarter, secure, and more connected future.</span>
+
         </p>
       </div>
       <div className="flex-1 flex justify-center">
